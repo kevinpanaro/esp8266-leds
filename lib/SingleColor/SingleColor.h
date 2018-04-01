9261d271f9dc5191792d78f262471f3131155821
Rémi Somdecoste-Lespoune <saponace@gmail.com>
2018-04-02 01:00:18 +0200

    Formatting

--- lib/SingleColor/SingleColor.h
+++ lib/SingleColor/SingleColor.h
@@ -1,5 +1,5 @@
-#ifndef SingleColor_H
-#define SingleColor_H
+#ifndef SINGLE_COLOR_H
+#define SINGLE_COLOR_H
 
 #include <ESP8266WebServer.h>
 #include "Error.h"
@@ -8,21 +8,21 @@ class SingleColor
 {
 
 private:
-    uint8_t red;
-    uint8_t green;
-    uint8_t blue;
-    void setColor(String colorCode, uint8_t value);
-    String buildValueOutOfRangeErrorMsg(String colorCode, uint8_t value) const;
+  uint8_t red;
+  uint8_t green;
+  uint8_t blue;
+  void setColor(String colorCode, uint8_t value);
+  String buildValueOutOfRangeErrorMsg(String colorCode, uint8_t value) const;
 
 public:
-    SingleColor();
-    SingleColor(ESP8266WebServer &server);
-    uint8_t getRed() const;
-    uint8_t getGreen() const;
-    uint8_t getBlue() const;
-    Error checkColorsValuesInRange() const;
-    void send(ESP8266WebServer &server);
-    String toString() const;
+  SingleColor();
+  SingleColor(ESP8266WebServer &server);
+  uint8_t getRed() const;
+  uint8_t getGreen() const;
+  uint8_t getBlue() const;
+  Error checkColorsValuesInRange() const;
+  void send(ESP8266WebServer &server);
+  String toString() const;
 };
 
 #endif
